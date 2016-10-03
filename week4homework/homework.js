2563bb9151433d1b409c3d360d96a246d536973d
dpfriar <dpfriar@gmail.com>
2016-10-03 18:56:42 -0400

    adding homework

---
+++ week4homework/homework.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+  
+  $(".readmore").on("click", function(event){ 
+    event.preventDefault();
+    $("#show-this-on-click").slideDown();
+    $(".readless").show();
+    $(".readmore").hide();
+  });
+
+$(".readless").on("click", function(event){ 
+    event.preventDefault();
+    $("#show-this-on-click").slideUp();
+    $(".readmore").show();
+    $(".readless").hide();
+  });
+
+ $(".learnmore").on("click", function(event){ 
+    event.preventDefault();
+    $("#learnmoretext").slideDown();
+     $(".readless").show();
+     $(".learnmore").hide();
+  });
+  
+});
+
+
+
+
+
